71c10a5bb57e53aaf53f650c55c61d9040bbbed5
Hamza <hamzabitup@gmail.com>
2020-09-05 11:55:58 +0100

    New http_server project

---
+++ http_server/target/debug/deps/http_server.d
@@ -0,0 +1,5 @@
+/Users/MarshMallow1/Desktop/work/Personal/Rust/http_server/target/debug/deps/http_server: src/main.rs
+
+/Users/MarshMallow1/Desktop/work/Personal/Rust/http_server/target/debug/deps/http_server.d: src/main.rs
+
+src/main.rs:
---
+++ http_server/target/debug/http_server.d
@@ -0,0 +1,1 @@
+/Users/MarshMallow1/Desktop/work/Personal/Rust/http_server/target/debug/http_server: /Users/MarshMallow1/Desktop/work/Personal/Rust/http_server/src/main.rs
